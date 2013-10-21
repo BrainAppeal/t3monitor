f01027739f2c0c8883cb178acf4c102379d4e1d4
Gert Hammes <gert.hammes@meteos.de>
2013-10-21 13:28:56 +0200

    [TASK] Updated version info and set minimum PHP version to 5.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "brainmonitor".
  *
- * Auto generated 11-06-2013 10:05
+ * Auto generated 21-10-2013 13:28
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'info@brain-appeal.com',
 	'author_company' => 'Brain Appeal GmbH',
 	'shy' => '',
-	'version' => '2.0.5',
+	'version' => '2.0.6',
 	'priority' => '',
 	'module' => '',
 	'state' => 'stable',
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'php' => '5.2.0-0.0.0',
+			'php' => '5.3.0-0.0.0',
 			'typo3' => '4.5.0-0.0.0',
 		),
 		'conflicts' => array(
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:21:"ext_conf_template.txt";s:4:"ec02";s:12:"ext_icon.gif";s:4:"a2ef";s:17:"ext_localconf.php";s:4:"c093";s:25:"Classes/Helper/Config.php";s:4:"122e";s:27:"Classes/Helper/Database.php";s:4:"8dfe";s:29:"Classes/Helper/Encryption.php";s:4:"88b5";s:25:"Classes/Helper/Logger.php";s:4:"4aa3";s:24:"Classes/Helper/Timer.php";s:4:"6056";s:28:"Classes/Reports/Abstract.php";s:4:"03e5";s:28:"Classes/Reports/Database.php";s:4:"59ba";s:24:"Classes/Reports/Disc.php";s:4:"973d";s:29:"Classes/Reports/Extension.php";s:4:"78a7";s:27:"Classes/Reports/Reports.php";s:4:"7300";s:28:"Classes/Reports/Security.php";s:4:"5231";s:34:"Classes/Reports/SecurityCompat.php";s:4:"b538";s:26:"Classes/Reports/Server.php";s:4:"6d33";s:26:"Classes/Reports/SysLog.php";s:4:"bdca";s:30:"Classes/Service/Dispatcher.php";s:4:"58af";s:40:"Resources/Private/Language/locallang.xml";s:4:"c434";s:43:"Resources/Private/Language/locallang_be.xml";s:4:"733e";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"125f";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";}',
+	'_md5_values_when_last_written' => 'a:23:{s:21:"ext_conf_template.txt";s:4:"ec02";s:12:"ext_icon.gif";s:4:"a2ef";s:17:"ext_localconf.php";s:4:"c093";s:25:"Classes/Helper/Config.php";s:4:"122e";s:27:"Classes/Helper/Database.php";s:4:"5b2e";s:29:"Classes/Helper/Encryption.php";s:4:"88b5";s:25:"Classes/Helper/Logger.php";s:4:"4aa3";s:24:"Classes/Helper/Timer.php";s:4:"6056";s:28:"Classes/Reports/Abstract.php";s:4:"03e5";s:28:"Classes/Reports/Database.php";s:4:"59ba";s:24:"Classes/Reports/Disc.php";s:4:"973d";s:29:"Classes/Reports/Extension.php";s:4:"b744";s:27:"Classes/Reports/Reports.php";s:4:"7300";s:28:"Classes/Reports/Security.php";s:4:"060c";s:34:"Classes/Reports/SecurityCompat.php";s:4:"b538";s:26:"Classes/Reports/Server.php";s:4:"6d33";s:26:"Classes/Reports/SysLog.php";s:4:"bdca";s:33:"Classes/Service/Compatibility.php";s:4:"cd0e";s:30:"Classes/Service/Dispatcher.php";s:4:"02c6";s:40:"Resources/Private/Language/locallang.xml";s:4:"c434";s:43:"Resources/Private/Language/locallang_be.xml";s:4:"733e";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"125f";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";}',
 );
 
 ?>
