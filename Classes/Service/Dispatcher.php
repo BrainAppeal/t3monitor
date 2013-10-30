e19db936ab23c86226385cd55b82499a64ab6d88
Gert Hammes <gert.hammes@meteos.de>
2013-10-30 17:01:17 +0100

    [TASK] Added function to search for Piwik installation on same server and reporting version

--- Classes/Service/Dispatcher.php
+++ Classes/Service/Dispatcher.php
@@ -40,6 +40,7 @@ require_once $tmpExtPath . 'Classes/Reports/SecurityCompat.php';
 require_once $tmpExtPath . 'Classes/Reports/Server.php';
 require_once $tmpExtPath . 'Classes/Reports/SysLog.php';
 require_once $tmpExtPath . 'Classes/Reports/Links.php';
+require_once $tmpExtPath . 'Classes/Reports/Applications.php';
 require_once $tmpExtPath . 'Classes/Service/Compatibility.php';
 /**
  * Main service class which creates and sends reports for this TYPO3 installation
@@ -142,6 +143,7 @@ class Tx_Brainmonitor_Service_Dispatcher
             'system' => 'Tx_Brainmonitor_Reports_Server',
             'disc' => 'Tx_Brainmonitor_Reports_Disc',
             'links' => 'Tx_Brainmonitor_Reports_Links',
+            'applications' => 'Tx_Brainmonitor_Reports_Applications',
         );
         $enabledReports = array();
         if(isset($params['reports'])) {
