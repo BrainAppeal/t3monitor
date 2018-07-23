056d2f0ce8321c3ea69f144720e02a34ed3fcf39
Giuseppe Cavallaro <giuseppe.cavallaro@brain-appeal.com>
2018-07-23 12:29:31 +0200

    updated typo3 requirements to 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '4.5.0-8.9.99',
+			'typo3' => '4.5.0-9.5.99',
 		),
 		'conflicts' => array(
 		),
